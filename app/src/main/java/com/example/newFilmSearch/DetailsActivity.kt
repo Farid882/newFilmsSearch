b10b4a884a7b37e07fa93af3218a6534b063ccb5
Farid Sabirjanow <subfarid783@gmail.com>
2025-05-26 22:06:10 +0300

    create film search project

---
+++ app/src/main/java/com/example/newFilmSearch/DetailsActivity.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityDetailsBinding
+
+class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //Устанавливаем тулбар
+        setSupportActionBar(binding.detailsToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        binding.detailsToolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        setFilmsDetails()
+    }
+
+    private fun setFilmsDetails() {
+        //Получаем наш фильм из переданного бандла
+        val film = intent.extras?.get("film") as Film
+        binding.detailsToolbar.title = film.title
+
+        binding.toolbarLayout.title = film.title
+        binding.detailsDescription.text = film.description
+        binding.detailsPoster.setImageResource(film.poster)
+        binding.detailsToolbar.title = film.title
+
+    }
+}
---
+++ app/src/main/java/com/example/newFilmSearch/FilmViewHolder.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.TextView
+import android.widget.ImageView
+
+class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    //Привязываем view из layout к переменным
+    private val title: TextView = itemView.findViewById(R.id.title)
+    private val poster: ImageView = itemView.findViewById(R.id.poster)
+    private val description: TextView = itemView.findViewById(R.id.description)
+
+    //В этом методе кладем данные из film в наши view
+    fun bind(film: Film) {
+        title.text = film.title
+        //Устанавливаем постер
+        poster.setImageResource(film.poster)
+        //Устанавливаем описание
+        description.text = film.description
+    }
+}
